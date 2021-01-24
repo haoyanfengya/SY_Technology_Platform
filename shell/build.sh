c9e5674d77bc444540ae460743a4f117c0be9afd
chester <wojiaoadou@126.com>
2021-01-24 23:21:27 +0800

    commit sql shell

--- shell/build.sh
+++ shell/build.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+
 git clone https://github.com/haoyanfengya/springboot.git
 
-cp springboot/sql/sql.sh ./
+cp springboot/shell/sql.sh ./
 cp -r springboot/sql/ ./
 
+cd springboot
+mvn install
+cd ..
+cp springboot/target/springboot_demo-0.0.1-SNAPSHOT.war ./
+rm -rf springboot
+
+docker build -t springboot .
+docker run -itd --name springboot -p 8081:8080 -e MYSQL_ROOT_PASSWORD=123456 springboot
